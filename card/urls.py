1013cb7b7f481e453d9a35e3bae4af0b1b21d094
pmiyan <pmiyan@tamu.edu>
2023-06-19 13:53:23 -0400

    added detail page for cards

--- card/urls.py
+++ card/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("hello", views.hello_world, name="hello-world"),
+    path("<int:card_id>", views.detail, name="card-detail"),
     path("", views.home, name="cards-home"),
 ]
--- card/views.py
+++ card/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import CardTitle, CardContent, CardType
 
 def hello_world(request):
     return HttpResponse("Hello World")
 
+def detail(request, card_id):
+    try:
+        context = {'card':CardTitle.objects.get(pk=card_id)}
+    except:
+        raise Http404("Card not found -____-")
+    return render(request, 'card/detail.html', context)
+
 def home(request):
-    context = {'cards': CardTitle.objects.all()}
+    try:
+        context = {'cards': CardTitle.objects.all()}
+    except:
+        raise Http404("Page not found -____-")
     return render(request, 'card/home.html', context)
